f5e4890296e6a724f9f82abce63b3a5ba142e7e1
elise-lapauw <55782687+elise-lapauw@users.noreply.github.com>
2020-07-10 14:36:42 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname, '/dist/angular-example-starter/src')));
+app.use(express.static(__dirname, 'dist', {index: false}));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname, '/dist/angular-example-starter/src/index.html'));
+
+
+// PathLocationStrategy
+
+app.get('', function(req, res) {
+    res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
+
 
 
 // Start the app by listening on the default Heroku port
