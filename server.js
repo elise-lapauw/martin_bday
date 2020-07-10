cef32177d34b063bf1f552646fa55590dd6b2224
elise-lapauw <55782687+elise-lapauw@users.noreply.github.com>
2020-07-10 14:24:11 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname, '/dist/angular-example-starter'));
+app.use(express.static(path.join(__dirname, '/dist/angular-example-starter/src')));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname, '/dist/angular-example-starter/index.html'));
+res.sendFile(path.join(__dirname, '/dist/angular-example-starter/src/index.html'));
 });
 
 
