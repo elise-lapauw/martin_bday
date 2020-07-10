0b28fea009505ba2dbd089c46f2399261c14b0e2
elise-lapauw <55782687+elise-lapauw@users.noreply.github.com>
2020-07-10 13:45:08 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -20,4 +20,4 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname,'/dist/angular-example-starter/index.html'));
 });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8081);
+app.listen(process.env.PORT || 8080);
